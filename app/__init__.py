631083e1c85538059326378a361e4cef8bcfb419
Nkosinam Dlungwana <139022461+Awakenned1@users.noreply.github.com>
2024-12-08 18:56:25 +0200

    first commit . no bugs

--- app/__init__.py
+++ app/__init__.py
@@ -5,22 +5,23 @@ from flask_mail import Mail
 db = SQLAlchemy()
 mail = Mail()
 
+
 def create_app():
     app = Flask(__name__)
-    
+
     # Load config
     app.config.from_object('app.config.Config')
-    
+
     # Initialize extensions
     db.init_app(app)
     mail.init_app(app)
-    
+
     with app.app_context():
         # Import routes
         from app.routes import main as main_blueprint
         app.register_blueprint(main_blueprint)
-        
+
         # Create database tables
         db.create_all()
-        
+
     return app
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 from datetime import datetime
 
+
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
